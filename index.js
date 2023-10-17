eac0f43fcf01679a5d697b076373950b6858b74a
M. Dhamar Florez <mabeldhamar@gmail.com>
2023-10-17 15:36:38 -0500

    add-to-html

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-import data from "./data.json" assert { type: 'json' };;
+import data from "./data.json" assert { type: 'json' };
 
 console.log(data)
 console.log(data.films)
+
+const dataFilms = data.films;
+
+
+
+const cardsHtml = dataFilms.map((element)=>{
+    return `<div class="image-wrapper">
+      <img src="${element.Images[0]}" alt="${element.Title}" />
+    </div>
+    <div class="content">
+      <h4>${element.Title}</h4>
+      <div class="movie-info">
+        <div class="score-wrapper">
+          <img src="./assets/star.svg" alt="Ícono de una estrella" />
+          <p class="score">${element.imdbRating}/10</p>
+        </div>
+        <span class="genre">${element.Genre}</span>
+      </div>
+      <p class="plot">${element.Plot}</p>
+    </div>`
+  }).join ("")
+  console.log(cardsHtml)
+  document.querySelector(".movie-item").innerHTML = cardsHtml
+ 
+  movieContainer.innerHTML = cardsHtml;
+
+
+
+
+
+
+
+
+
