77528ff6449a3962d95229e613378002225a422f
Angelica Agudelo <aagudelo482@soyudemedellin.edu.co>
2023-10-18 07:57:52 -0500

    entrega 3.2 (trabajo clase)

--- index.js
+++ index.js
@@ -25,3 +25,39 @@ const filmsHtml = data.films.map((element) => {
 }).join("")
 
 document.getElementsByClassName("movies-container")[0].innerHTML = filmsHtml
+
+document.querySelectorAll(".filter-button").forEach((buttonElement) => {
+  buttonElement.addEventListener("click", (e) => {
+    console.log(e.target.textContent);
+    const selectedType = e.target.textContent;
+    const newFilms = data.films.filter(
+      (element) => element.Genre.includes(selectedType)
+    );
+    console.log(newFilms)
+
+    const newfilmsHtml = newFilms.map((element) => {
+      return `
+      <article class="movie-item">
+      <div class="image-wrapper">
+        <img src="${element.Images[0]}">
+      </div>
+      <div class="content">
+        <h4>${element.Title}</h4>
+        <div class="movie-info">
+          <div class="score-wrapper">
+            <img src="./assets/star.svg" alt="Ícono de una estrella" />
+            <p>${element.imdbRating}/10</p>
+          </div>
+          <span class="genre">${element.Genre}</span>
+        </div>
+        <p class="plot">${element.Plot}</p>
+      </div>
+    </article>
+      `
+    }).join("")
+    
+    document.getElementsByClassName("movies-container")[0].innerHTML = newfilmsHtml
+  });
+});
+
+
