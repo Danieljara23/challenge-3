3d5b630ae15cb5a2e340bd9cdda846f0a36b418e
Santiago A <santiagoaguirreb6@gmail.com>
2023-10-18 08:50:43 -0500

    Hito 2

--- index.js
+++ index.js
@@ -26,7 +26,45 @@ const infoCards = data.films.map((element) => {
     `;
   }).join("");
 
-document.getElementsByClassName("movies-container")[0].innerHTML = infoCards
+document.getElementsByClassName("movies-container")[0].innerHTML = infoCards;
+
+const btn = document.querySelectorAll(".filter-button");
+
+btn.forEach((btnelement) => {
+  btnelement.addEventListener("click", (e)  => {
+    console.log(e.target);
+    const selectBtn = e.target.innerText;
+    const filterBtn = data.films.filter((filmElement) => filmElement.Genre.toLowerCase().includes(selectBtn.toLowerCase()));
+    console.log(filterBtn);
+
+    const films = filterBtn.map((element) => {
+      return `
+      <article class="movie-item">
+      <div class="image-wrapper">
+        <img
+          src="${element.Images}"
+          alt=""
+        />
+      </div>
+      <div class="content">
+        <h4>${element.Title}</h4>
+        <div class="movie-info">
+          <div class="score-wrapper">
+            <img src="./assets/star.svg" alt="Ícono de una estrella" />
+            <p class="score">${element.imdbRating}</p>
+          </div>
+          <span class="genre">${element.Genre}</span>
+        </div>
+        <p class="plot">
+          ${element.Plot}
+        </p>
+      </div>
+      </article>
+      `;
+    }).join("");
+    document.getElementsByClassName("movies-container")[0].innerHTML = films;
+  });
+});
 
 console.log(data)
 console.log(data.films)
