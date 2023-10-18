f013dbd684bf3f7333f3f6f51e5efc7b2b78b1b7
Valuv <valuv027@gmail.com>
2023-10-18 06:25:00 -0500

    <Final Challenge- 3>

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-import data from "./data.json" assert { type: 'json' };
+import data from "./data.json" assert { type: 'json' };;
 
 console.log(data)
 console.log(data.films)
 
 const filmsHtml = data.films.map((element) => {
     return `
-    <div class="image-wrapper">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/M/MV5BNzM2MDk3MTcyMV5BMl5BanBnXkFtZTcwNjg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg"
-        alt=""
-      />
-    </div>
-    <div class="content">
-      <h4>${element.Title}</h4>
-      <div class="movie-info">
-        <div class="score-wrapper">
-          <img src="./assets/star.svg" alt="Ícono de una estrella" />
-          <p class="score">9.0/10</p>
-        </div>
-        <span class="genre">Suspend</span>
+    <article class="movie-item">
+      <div class="image-wrapper">
+        <img
+          src=${element.Poster}
+          alt="Movie Poster"
+        />
       </div>
-      <p class="plot">
-        ${element.Plot}
-      </p>
-    </div> 
+      <div class="content">
+        <h4>${element.Title}</h4>
+        <div class="movie-info">
+          <div class="score-wrapper">
+            <img src="./assets/star.svg" />
+            <p class="score"${element.imdbRating}/10</p>
+          </div>
+          <span class="genre">${element.Genre}</span>
+        </div>
+        <p class="plot">
+          ${element.Plot}
+        </p>
+      </div> 
+    </article>
     `
 }).join("")
 
-document.getElementsByClassName("movies-container").innerHTML = filmsHtml
+document.getElementById("container").innerHTML = filmsHtml
