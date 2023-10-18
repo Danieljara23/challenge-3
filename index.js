37fe332f377332d7698edee7d1c32b48c2473b87
Victoria Rosenstiehl <tori.roda@gmail.com>
2023-10-17 23:22:52 -0500

    Hito 1 done

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-import data from "./data.json" assert { type: 'json' };;
 
-console.log(data)
-console.log(data.films)
+import data from "./data.json"assert { type: 'json' };;
+
+console.log(data);
+console.log(data.films);
+
+const movieHtml = data.films.map((filmsElement) => {
+  return `
+    <article class="movie-item">
+      <div class="content">
+      <div class="image-wrapper">
+            <img
+              src="${filmsElement.Images}"
+              alt=""
+            />
+        <h4>${filmsElement.Title}</h4>
+        <div class="score-wrapper">
+        <img src="./assets/star.svg" alt="Ícono de una estrella" />
+        <p class="score">${filmsElement.imdbRating}</p>
+        <span class="genre">${filmsElement.Genre}</span>
+        </div>
+        <p class="plot"> ${filmsElement.Plot}</p>
+      </div>
+    </article>
+  `;
+}).join("");
+
+const moviesContainer = document.querySelector(".movies-container");
+
+if (moviesContainer) {
+  moviesContainer.innerHTML = movieHtml;
+} else {
+  console.error("Element with class 'movies-container' not found.");
+}
