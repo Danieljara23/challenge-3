7109429d938a424b47377e58639a49724cc90b18
Iván Jiménez <ivandariojimenezmira@gmail.com>
2023-10-18 07:16:29 -0500

    Tajetas de las películas actualizadas con la base de datos de json

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 import data from "./data.json" assert { type: 'json' };;
 
+const movies = data.films.map((element) => {
+  return `
+      <article class="movie-item">
+          <div class="image-wrapper">
+              <img
+                  src=${element.Images}
+                  alt=""
+              />
+          </div>
+          <div class="content">
+          <h4>${element.Title}</h4>
+          <div class="movie-info">
+              <div class="score-wrapper">
+                  <img src="./assets/star.svg" alt="Ícono de una estrella" />
+                  <p class="score">${element.imdbRating}</p>
+              </div>
+              <span class="genre">${element.Genre}</span>
+          </div>
+          <p class="plot">
+            ${element.Plot}
+          </p>
+          </div>
+      </article>
+    `;
+}).join("");
+
+document.getElementsByClassName("movies-container")[0].innerHTML = movies
+
 console.log(data)
 console.log(data.films)
+
