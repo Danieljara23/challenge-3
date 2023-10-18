22d686b5ce97b9ecb73d7be4f7f0bc3cb98fa518
SoulKingSc <simonkyo28@gmail.com>
2023-10-18 08:11:08 -0600

    uwu

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import data from "./data.json" assert { type: 'json' };;
 
 const infoCards = data.films.map((element) => {
-    return`
+  return `
     <article class="movie-item">
           <div class="image-wrapper">
             <img
@@ -14,9 +14,9 @@ const infoCards = data.films.map((element) => {
             <div class="movie-info">
               <div class="score-wrapper">
                 <img src="./assets/star.svg" alt="Ícono de una estrella" />
-                <p class="score">9.0/10</p>
+                <p class="score">${element.imdbRating}</p>
               </div>
-              <span class="genre">Suspend</span>
+              <span class="genre">${element.Genre}</span>
             </div>
             <p class="plot">
              ${element.Plot}
@@ -29,3 +29,39 @@ const infoCards = data.films.map((element) => {
 document.getElementsByClassName("movies-container")[0].innerHTML = infoCards
 console.log(data)
 console.log(data.films)
+document.querySelectorAll(".filter-button").forEach((buttonElement) => {
+  buttonElement.addEventListener("click", (e) => {
+    console.log(e.target.tex)
+    const selectedButtonText = e.target.textContent;
+    const filteredTasks = data.films.filter((filmElement) => filmElement.Genre.toLowerCase().includes(selectedButtonText.toLowerCase()))
+    console.log(filteredTasks)
+
+    const newInfoCards = filteredTasks.map((buttonElement) => {
+      return `
+    <article class="movie-item">
+          <div class="image-wrapper">
+            <img
+              src=${buttonElement.Images[0]}
+              alt=""
+            />
+          </div>
+          <div class="content">
+            <h4>${buttonElement.Title}</h4>
+            <div class="movie-info">
+              <div class="score-wrapper">
+                <img src="./assets/star.svg" alt="Ícono de una estrella" />
+                <p class="score">${buttonElement.imdbRating}</p>
+              </div>
+              <span class="genre">${buttonElement.Genre}</span>
+            </div>
+            <p class="plot">
+             ${buttonElement.Plot}
+            </p>
+          </div>
+        </article>
+        `;
+
+    }).join("");
+    document.getElementsByClassName("movies-container")[0].innerHTML = newInfoCards
+  })
+})
