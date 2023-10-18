271fe8d1c0a1f3bb14680a2faf1af176880b693e
M. Dhamar Florez <mabeldhamar@gmail.com>
2023-10-18 07:53:44 -0500

    checkpoint-2

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import data from "./data.json" assert { type: 'json' };
 
-console.log(data)
-console.log(data.films)
-
 const cardsHtml = data.films.map((element)=>{
-    return `<div class="image-wrapper">
+    return ` <article class="movie-item">
+    <div class="image-wrapper">
       <img src="${element.Images[0]}" alt="${element.Title}" />
     </div>
     <div class="content">
@@ -17,11 +15,41 @@ const cardsHtml = data.films.map((element)=>{
         <span class="genre">${element.Genre}</span>
       </div>
       <p class="plot">${element.Plot}</p>
-    </div>`
+    </div>
+    </article>`
   }).join ("")
-  console.log(cardsHtml)
- document.querySelector(".movies-container").innerHTML = cardsHtml  
+ document.querySelector(".movies-container").innerHTML = cardsHtml     
  
+ document.querySelectorAll(".filter-button").forEach((buttonElement) => {
+  buttonElement.addEventListener("click", (event) => {
+    const selectedType = event.target.textContent;
+    const cardsfilter = data.films.filter(
+      (element) => element.Genre.includes(selectedType) 
+    );
+    const newfilmsHtml = cardsfilter.map((element) => {
+      return ` <article class="movie-item">
+      <div class="image-wrapper">
+        <img src="${element.Images[0]}" alt="${element.Title}" />
+      </div>
+      <div class="content">
+        <h4>${element.Title}</h4>
+        <div class="movie-info">
+          <div class="score-wrapper">
+            <img src="./assets/star.svg" alt="Ícono de una estrella" />
+            <p class="score">${element.imdbRating}/10</p>
+          </div>
+          <span class="genre">${element.Genre}</span>
+        </div>
+        <p class="plot">${element.Plot}</p>
+      </div>
+      </article>`
+    }).join ("")
+
+   document.querySelector(".movies-container").innerHTML = newfilmsHtml   
+
+
+  });
+}); 
 
 
  
